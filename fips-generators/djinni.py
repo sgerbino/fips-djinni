640e93d1117a6cf1b4deb5e2e4bf9e0870dbcfd4
Steve Gerbino <steve@gerbino.co>
2016-10-07 01:06:30 -0400

    Removed version because it probably shouldn't be used with djinni, fixed up default arguments it's expected to be a dictionary

--- fips-generators/djinni.py
+++ fips-generators/djinni.py
@@ -3,46 +3,42 @@ import yaml
 from subprocess import call
 import genutil as util
 
-version = 1
-
 djinni = os.path.abspath(os.path.join(os.path.realpath(os.path.dirname(__file__)), '..', 'djinni', 'src', 'run'))
 
-default_args = ("{"
-                "java-out: java,"
-                "cpp-out: src/interface,"
-                "objc-out:  objc,"
-                "objcpp-out: objc,"
-                "jni-out: jni,"
-                "objcpp-include-cpp-prefix: interface/,"
-                "cpp-optional-header: \"<experimental/optional>\","
-                "cpp-optional-template: std::experimental::optional,"
-                "cpp-namespace: mobilepp,"
-                "java-package: io.github.mobilepp,"
-                "jni-namespace: mobilepp_jni,"
-                "objc-type-prefix: MPP,"
-                "objcpp-namespace: mobilecpp_obj,"
-                "ident-cpp-enum: foo_bar,"
-                "ident-cpp-field: foo_bar,"
-                "ident-cpp-method: foo_bar,"
-                "ident-cpp-enum-type: foo_bar,"
-                "ident-cpp-type-param: foo_bar,"
-                "ident-cpp-local: foo_bar,"
-                "ident-cpp-file: foo_bar,"
-                "ident-cpp-type: foo_bar,"
-                "ident-java-field: mFooBar,"
-                "ident-jni-class: NativeFooBar,"
-                "ident-jni-file: native_foo_bar"
-                "}")
+default_args = yaml.load("{"
+                         "java-out: java,"
+                         "cpp-out: src/interface,"
+                         "objc-out:  objc,"
+                         "objcpp-out: objc,"
+                         "jni-out: jni,"
+                         "objcpp-include-cpp-prefix: interface/,"
+                         "cpp-optional-header: \"<experimental/optional>\","
+                         "cpp-optional-template: \"std::experimental::optional\","
+                         "cpp-namespace: mobilepp,"
+                         "java-package: io.github.mobilepp,"
+                         "jni-namespace: mobilepp_jni,"
+                         "objc-type-prefix: MPP,"
+                         "objcpp-namespace: mobilecpp_obj,"
+                         "ident-cpp-enum: foo_bar,"
+                         "ident-cpp-field: foo_bar,"
+                         "ident-cpp-method: foo_bar,"
+                         "ident-cpp-enum-type: foo_bar,"
+                         "ident-cpp-type-param: foo_bar,"
+                         "ident-cpp-local: foo_bar,"
+                         "ident-cpp-file: foo_bar,"
+                         "ident-cpp-type: foo_bar,"
+                         "ident-java-field: mFooBar,"
+                         "ident-jni-class: NativeFooBar,"
+                         "ident-jni-file: native_foo_bar"
+                         "}")
 
 def generate(input, out_src, out_hdr, args = default_args):
-        if util.isDirty(version, [input], []):
-                cmd = []
-                cmd.append(djinni)
-                cmd.append('--idl')
-                cmd.append(input)
-                djinni_args = yaml.load(args)
-                for dictionary in djinni_args:
-                        for key, value in dictionary.items():
-                                cmd.append('--{}'.format(key))
-                                cmd.append(value)
-                call(cmd)
+                if util.isDirty(None, [input], []):
+                                cmd = []
+                                cmd.append(djinni)
+                                cmd.append('--idl')
+                                cmd.append(input)
+                                for key, value in args.items():
+                                                cmd.append('--{}'.format(key))
+                                                cmd.append(value)
+                                call(cmd)
